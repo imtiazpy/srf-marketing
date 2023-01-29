7d5ac3c6d30239408d40ea1afb3423060175cc23
imtiazpy <ahmed.imti1994@gmail.com>
2023-01-29 23:45:12 +0600

    working on hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import { NextPageWithLayout } from './page'
 import Layout from '@/components/Layout/Layout'
+import Hero from '@/components/Hero/Hero'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home: NextPageWithLayout = () => {
   return (
-    <div className='text-3xl'>
-      I'm from home
-    </div>
+    <>
+      <Hero />
+    </>
   );
 };
 
