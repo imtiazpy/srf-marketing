be0823e8d9a65f698718bdecd0754c00ad06e9d3
imtiazpy <ahmed.imti1994@gmail.com>
2023-02-03 18:24:03 +0600

    used static image for teamCard in OurTeam and removed CardFront,CardBack component and merge them in TeamCard

--- src/components/OurTeam/OurTeam.tsx
+++ src/components/OurTeam/OurTeam.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import TeamCard from './TeamCard';
 import styles from './OurTeam.module.css';
 
+import alexanderPic from '../../../public/alexander.jpg';
+import alexPic from '../../../public/alex-suprun.jpg';
+import julianPic from '../../../public/julian.jpg';
+import oguzPic from '../../../public/oguz.jpg';
+
 export interface IOurTeam extends React.ComponentPropsWithoutRef<'section'> {
 
 }
@@ -16,13 +21,13 @@ const OurTeam: React.FC<IOurTeam> = ({ className, ...sectionProps }) => {
       </div>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
         
-        <TeamCard image='/alexander.jpg' name='Alexander' designation='Web Designer' bio="Web design is more than just aesthetics for this creative mastermind. With a deep understanding of user experience and a passion for clean, functional design, this web designer crafts online experiences that not only look good but also deliver results." />
+        <TeamCard image={alexanderPic} name='Alexander' designation='Web Designer' bio="Web design is more than just aesthetics for this creative mastermind. With a deep understanding of user experience and a passion for clean, functional design, this web designer crafts online experiences that not only look good but also deliver results." />
 
-        <TeamCard image='/alex-suprun.jpg' name='Alex Suprun' designation='Graphic Designer' bio='From logos and branding materials to web and print designs, this designer has a proven track record of creating impactful work for clients across multiple industries. When not at the computer, you can find this designer exploring the city for inspiration, sketching in a notebook, or trying out new design software.' />
+        <TeamCard image={alexPic} name='Alex Suprun' designation='Graphic Designer' bio='From logos and branding materials to web and print designs, this designer has a proven track record of creating impactful work for clients across multiple industries. When not at the computer, you can find this designer exploring the city for inspiration, sketching in a notebook, or trying out new design software.' />
 
-        <TeamCard image='/julian.jpg' name='Julian' designation='UI/UX Designer' bio='With a background in psychology and a passion for design, this UI/UX designer creates digital products that are both visually stunning and user-friendly. From researching user behavior to prototyping and testing, this designer approaches each project with a user-centered mindset to ensure that the final product meets the needs of its target audience.' />
+        <TeamCard image={julianPic} name='Julian' designation='UI/UX Designer' bio='With a background in psychology and a passion for design, this UI/UX designer creates digital products that are both visually stunning and user-friendly. From researching user behavior to prototyping and testing, this designer approaches each project with a user-centered mindset to ensure that the final product meets the needs of its target audience.' />
 
-        <TeamCard image='/oguz.jpg' name='Oguz' designation='Web Developer' bio="Code is the canvas for this web developer, who brings websites and web applications to life with their mastery of HTML, CSS, JavaScript, and beyond. With a focus on clean, efficient code and a passion for innovation, this developer is always pushing the boundaries of what's possible on the web." />
+        <TeamCard image={oguzPic} name='Oguz' designation='Web Developer' bio="Code is the canvas for this web developer, who brings websites and web applications to life with their mastery of HTML, CSS, JavaScript, and beyond. With a focus on clean, efficient code and a passion for innovation, this developer is always pushing the boundaries of what's possible on the web." />
 
       </div>
     </section>
--- src/components/OurTeam/TeamCard.tsx
+++ src/components/OurTeam/TeamCard.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import React, { useState } from 'react';
 
 import styles from './TeamCard.module.css';
 
 export interface ITeamCard extends React.ComponentPropsWithoutRef<'div'> {
-  image: string;
+  image: StaticImageData;
   name: string;
   designation?: string;
   bio?: string;
@@ -22,9 +22,27 @@ const TeamCard: React.FC<ITeamCard> = ({ image, name, designation, bio, classNam
 
       <div onClick={flip} className={`relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] ${flipped ? '[transform:rotateY(180deg)]' : ''}`}>
 
-        <CardFront image={image} name={name} designation={designation} />
+        {/* Card Front */}
+        <div className={`absolute inset-0 flex flex-col items-center border-2 border-purple-600 rounded-lg text-white py-4 gap-4 shadow-lg shadow-red-500/50 cursor-pointer ${styles.cardFront}`}>
+          <div className='w-48 h-48 relative'>
+            <Image src={image} className='rounded-full object-cover' fill sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' alt='Photo of Team member' placeholder='blur' />
+          </div>
+          <div className='flex flex-col items-center gap-2'>
+            <h4>{name}</h4>
+            <p>{designation}</p>
+            <button className='text-xs border rounded-lg p-1 hover:bg-yellow-700'>See Bio</button>
+          </div>
+        </div>
 
-        <CardBack image={image} name={name} designation={designation} bio={bio} />
+        {/* Card Back */}
+        <div className={`absolute inset-0 flex flex-col items-center justify-center border-2 border-purple-600 rounded-lg text-white py-4 gap-4 shadow-lg shadow-red-500/50 cursor-pointer [transform:rotateY(180deg)] ${styles.cardBack}`}>
+          <div className="flex flex-col items-center justify-start px-1 gap-2 mt-2">
+            <h1 className="text-xl font-bold">{name}</h1>
+            <p className="text-base">{bio}</p>
+          </div>
+          <button className="absolute top-1 left-1 mt-2 rounded-md bg-neutral-800 py-1 px-2 hover:bg-neutral-900 text-xs">Go Back</button>
+
+        </div>
 
       </div>
 
@@ -34,33 +52,3 @@ const TeamCard: React.FC<ITeamCard> = ({ image, name, designation, bio, classNam
 };
 
 export default TeamCard;
-
-
-const CardFront: React.FC<ITeamCard> = ({ image, name, designation, bio, className, ...divProps }) => {
-  return (
-    <div className={`absolute inset-0 flex flex-col items-center border-2 border-purple-600 rounded-lg text-white py-4 gap-4 shadow-lg shadow-red-500/50 cursor-pointer ${styles.cardFront}`}>
-      <div className='w-48 h-48 relative'>
-        <Image src={image} className='rounded-full object-cover' fill sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw' alt='Photo of Team member' />
-      </div>
-      <div className='flex flex-col items-center gap-2'>
-        <h4>{name}</h4>
-        <p>{designation}</p>
-        <button className='text-xs border rounded-lg p-1 hover:bg-yellow-700'>See Bio</button>
-      </div>
-    </div>
-  )
-}
-
-
-const CardBack: React.FC<ITeamCard> = ({ image, name, designation, bio, className, ...divProps }) => {
-  return (
-    <div className={`absolute inset-0 flex flex-col items-center justify-center border-2 border-purple-600 rounded-lg text-white py-4 gap-4 shadow-lg shadow-red-500/50 cursor-pointer [transform:rotateY(180deg)] ${styles.cardBack}`}>
-      <div className="flex flex-col items-center justify-start px-1 gap-2 mt-2">
-        <h1 className="text-xl font-bold">{name}</h1>
-        <p className="text-base">{bio}</p>
-      </div>
-      <button className="absolute top-1 left-1 mt-2 rounded-md bg-neutral-800 py-1 px-2 hover:bg-neutral-900 text-xs">Go Back</button>
-
-    </div>
-  )
-}
