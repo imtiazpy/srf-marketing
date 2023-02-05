320c4faa01873fecd31b8bd9664bb3c7126caf42
imtiazpy <ahmed.imti1994@gmail.com>
2023-02-05 19:49:41 +0600

    width for responsiveness

--- src/components/OurClient/OurClient.tsx
+++ src/components/OurClient/OurClient.tsx
@@ -10,7 +10,7 @@ export interface IOurClient extends React.ComponentPropsWithoutRef<'section'> {
 const OurClient: React.FC<IOurClient> = ({ className, ...sectionProps }) => {
   return (
     <section>
-      <div className='flex items-center justify-around flex-wrap gap-4 py-10 px-20'>
+      <div className='flex items-center justify-around flex-wrap gap-4 py-10 px-2 sm:px-10 md:px-20'>
         <Image style={{width: 'auto', height: 'auto'}} src='/logoipsum-284.svg' alt='company logo' width={80} height={20} />
         <Image style={{width: 'auto', height: 'auto'}} src='/logoipsum-285.svg' alt='company logo' width={150} height={20} />
         <Image style={{width: 'auto', height: 'auto'}} src='/logoipsum-286.svg' alt='company logo' width={150} height={20} />
@@ -18,7 +18,7 @@ const OurClient: React.FC<IOurClient> = ({ className, ...sectionProps }) => {
         <Image style={{width: 'auto', height: 'auto'}} src='/logoipsum-290.svg' alt='company logo' width={80} height={20} />
         <Image style={{width: 'auto', height: 'auto'}} src='/logoipsum-256.svg' alt='company logo' width={80} height={20} />
       </div>
-      <div className={`${styles.bgImage} px-20 flex items-center justify-center md:justify-between flex-wrap gap-4`}>
+      <div className={`${styles.bgImage} flex items-center justify-center md:justify-between flex-wrap gap-4 px-2 sm:px-10 md:px-20`}>
         <Info title='3000+' info='Happy Clients' />
         <Info title='40+' info='Million Revenue' />
         <Info title='5000+' info='Completed Projects' />
